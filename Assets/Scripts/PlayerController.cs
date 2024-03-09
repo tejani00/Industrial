12f52142fff74e196dc507e1b36c21884713de70
TejaniBrijesh <tejanibrijesh123@gmail.com>
2024-03-09 18:06:57 +0530

    Money add

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator animator;
+
+    private bool temp = false;
+
+    void Update()
+    {
+        PlayerMovement();
+
+        PlayerJump();
+
+        FaceDirection();
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void PlayerMovement()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (!IsGrounded()) return;
+        if (horizontal != 0)
+        {
+            AnimPlay(1);
+        }
+        else
+        {
+            AnimPlay(0);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+
+            animator.Play("Anim_Jump");
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private void FaceDirection()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private void AnimPlay(float value)
+    {
+        animator.SetFloat("Blend", value);
+    }
+}
